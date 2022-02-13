d57b15770964d5cf968b093873e27354a2c15933
Volker Ströbel <volkerstroebel@mysurdity.de>
2022-02-13 11:35:00 +0100

    Add fuzz targets for subsampling

---
+++ fuzz/fuzz_targets/encode_rgb_subsampled.rs
@@ -0,0 +1,20 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+
+use jpeg_encoder::*;
+
+fuzz_target!(|data: &[u8]| {
+
+    let pixels = data.len() / 3;
+
+    let width = (pixels as f64).sqrt() as u16;
+    let height = width;
+
+    if width >0 && width < u16::MAX &&  height >0 && height < u16::MAX  {
+        let mut out = Vec::new();
+        let mut encoder = Encoder::new(&mut out, 80);
+        encoder.set_sampling_factor(SamplingFactor::F_4_2);
+        encoder.encode(data, width as u16, height as u16, ColorType::Rgb).unwrap();
+    }
+});
